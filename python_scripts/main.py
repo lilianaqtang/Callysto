303485fa6ce74ac8eaec5ef537068060a917fa57
root <root@EDML039.chandos.com>
2020-12-12 17:12:24 -0700

    added current work

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -15,7 +15,15 @@ import plotly.graph_objects as go
 from IPython.display import HTML
 import codecs
 import re
-
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, PathPatch, Rectangle
+from matplotlib.text import TextPath
+from matplotlib.transforms import Affine2D
+# This import registers the 3D projection, but is otherwise unused.
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
+import mpl_toolkits.mplot3d.art3d as art3d
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
 
 def choose_subject(option):
     intro_text=widgets.HTML(value=" ")
@@ -24,10 +32,46 @@ def choose_subject(option):
          display(Markdown("<br> *Introductory blurb about architecture from architecture team. Use a graphic from Pulp team.*"))
          display(Markdown("#### <span style='color:blue'>Variables in architecture"))
          display(Markdown("Select building size:"))
-         interact_manual(construct_building, 
-         x=widgets.FloatSlider(min=1, max=20, step=1, value=1,description='Length: X(m)',style=style), 
+         interact_manual(construct_building,
+         x=widgets.FloatSlider(min=1, max=20, step=1, value=1,description='Length: X(m)',style=style),
          y=widgets.FloatSlider(min=1, max=20, step=1, value=1,description='Width: Y(m)',style=style),
          z=widgets.FloatSlider(min=1, max=20, step=1, value=1,description='Height: Z(m)',style=style));
+         display(Markdown("Select building & window sizes:"))
+         display(Markdown("<br> *(all windows are set to same, and only 0 or 1 per wall)*"))
+         interact_manual(choose_direction,
+             direction=widgets.RadioButtons(
+                 options=['North(N)', 'South(S)', 'West(W)', 'East(E)'],
+                 value='West(W)',
+                 description='Window on N, E, S, W wall?',
+                 disabled=False,
+             ),
+            x=widgets.FloatSlider(min=1, max=20, step=1, value=1, description='Length: X(m)', style=style),
+            y=widgets.FloatSlider(min=1, max=20, step=1, value=1, description='Width: Y(m)', style=style),
+            z=widgets.FloatSlider(min=1, max=20, step=1, value=1, description='Height: Z(m)', style=style),
+            x1=widgets.FloatSlider(min=1, max=10, step=1, value=1, description='Length of the window: X(m)', style=style),
+            y1=widgets.FloatSlider(min=1, max=10, step=1, value=1, description='Height of the window: Y(m)', style=style),
+            z1=widgets.FloatSlider(min=1, max=10, step=1, value=1, description='Thickness of the window: Z(m)', style=style));
+         display(Markdown("W2W Ratio (window area : wall area)   (interactive calculation) m2  : (interactive calculation) m2 "))
+
+         display(Markdown("Insulation (thickness of wall)"))
+         display(Markdown("<br> *(Display only one wall)*"))
+
+         display(Markdown("Insulation (glass specification)"))
+         display(Markdown("<br> *(Display only one wall)*"))
+
+         display(Markdown("Insulation (roof specification)"))
+         display(Markdown("<br> *(Display building with shaded roof)*"))
+
+         display(Markdown("#### <span style='color:blue'>Better Building Calculations"))
+         display(Markdown("Cost to build (interactive calculation) $"))
+         display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
+
+         display(Markdown("Heat Loss (interactive calculation) kW"))
+         display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
+
+         display(Markdown("Heating Intensity (Peak Thermal Load)  (interactive calculation) W/m2"))
+         display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
+         display(Markdown("<br> *blurb from architecture team about what desirable aim is for a sustainable building*"))
 
     elif option == 'Biology':
          display(Markdown("### <span style='color:blue'>BIOLOGY FOCUS SELECTED"))
@@ -36,6 +80,9 @@ def choose_subject(option):
          display(Markdown("### <span style='color:blue'>COMPUTER SCIENCE FOCUS SELECTED"))
     elif option == 'Math':
          display(Markdown("### <span style='color:blue'>MATH FOCUS SELECTED"))
+         display(Markdown("The notebook serves as math lab and will consist of several “Trials”, each having the same set up. Students will progress through the lab, conducting each trial as instructed."))
+         display(Markdown("#### <span style='color:blue'>Trial 1. "))
+         display(Markdown("##### <span style='color:blue'>Modelling the Environment "))
     elif option == 'Physics':
          display(Markdown("### <span style='color:blue'>PHYSICS FOCUS SELECTED"))
 
@@ -78,34 +125,85 @@ def construct_building(x, y, z):
 ])
 
     fig.show()
-    display(Markdown("Building faces which direction? (aspect)"))
-    display(Markdown("<br> *Display the whole building, add aspect labels – allow rotation*"))
 
-    display(Markdown("Window on N, E, S, W wall?"))
-    display(Markdown("<br> *toggle for windows on each N, E, S, W wall*"))
 
-    display(Markdown("Change size of window"))
-    display(Markdown("<br> *(all windows are set to same, and only 0 or 1 per wall)*"))
 
-    display(Markdown("W2W Ratio (window area : wall area)   (interactive calculation) m2  : (interactive calculation) m2 "))
-    
-    display(Markdown("Insulation (thickness of wall)"))
-    display(Markdown("<br> *(Display only one wall)*"))
+def choose_direction(direction, x, y, z, x1, y1, z1):
+    if direction == 'West(W)':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
 
-    display(Markdown("Insulation (glass specification)"))
-    display(Markdown("<br> *(Display only one wall)*"))
-    
-    display(Markdown("Insulation (roof specification)"))
-    display(Markdown("<br> *(Display building with shaded roof)*"))
+        # Color the floor area
+        p = Rectangle((0, 0, 0), x, y, fc='gray')
+        ax.add_patch(p)
+        art3d.pathpatch_2d_to_3d(p, z=0, zdir="z")
 
-    display(Markdown("#### <span style='color:blue'>Better Building Calculations"))
-    display(Markdown("Cost to build (interactive calculation) $"))
-    display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
+        # Draw a rectangle on the x=0 'wall'
+        p1 = Rectangle((x1, y1), z / 2, y / 2, fc='none', ec='g', lw=z1)
+        ax.add_patch(p1)
+        art3d.pathpatch_2d_to_3d(p1, z=0, zdir="x")
 
-    display(Markdown("Heat Loss (interactive calculation) kW"))
-    display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
+        ax.set_xlim(0, x)
+        ax.set_ylim(0, y)
+        ax.set_zlim(0, z)
 
-    display(Markdown("Heating Intensity (Peak Thermal Load)  (interactive calculation) W/m2"))
-    display(Markdown("<br> *blurb from architecture team to explain calculation  and why it matters*"))
-    display(Markdown("<br> *blurb from architecture team about what desirable aim is for a sustainable building*"))
+        plt.show()
+    elif direction == 'North(N)':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.view_init(60)
 
+        # Color the floor area
+        p = Rectangle((0, 0, 0), x, y, fc='gray')
+        ax.add_patch(p)
+        art3d.pathpatch_2d_to_3d(p, z=0, zdir="z")
+
+        # Draw a rectangle on the x=0 'wall'
+        p1 = Rectangle((x1, y1), z / 2, y / 2, fc='none', ec='g', lw=z1)
+        ax.add_patch(p1)
+        art3d.pathpatch_2d_to_3d(p1, z=0, zdir="x")
+
+        ax.set_xlim(0, x)
+        ax.set_ylim(0, y)
+        ax.set_zlim(0, z)
+
+        plt.show()
+        # Windows
+    elif direction == 'East(E)':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        # Color the floor area
+        p = Rectangle((0, 0, 0), x, y, fc='gray')
+        ax.add_patch(p)
+        art3d.pathpatch_2d_to_3d(p, z=0, zdir="z")
+
+        # Draw a rectangle on the x=1 'wall'
+        p2 = Rectangle((x1, y1), x/2, z/2, fc='none', ec='g', lw=z1)
+        ax.add_patch(p2)
+        art3d.pathpatch_2d_to_3d(p2, z=4, zdir="y")
+
+        ax.set_xlim(0, x)
+        ax.set_ylim(0, y)
+        ax.set_zlim(0, z)
+    elif direction == 'South(S)':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.view_init(400)
+
+        # Color the floor area
+        p = Rectangle((0, 0, 0), x, y, fc='gray')
+        ax.add_patch(p)
+        art3d.pathpatch_2d_to_3d(p, z=0, zdir="z")
+
+        # Draw a rectangle on the x=0 'wall'
+        p1 = Rectangle((x1, y1), z / 2, y / 2, fc='none', ec='g', lw=z1)
+        ax.add_patch(p1)
+        art3d.pathpatch_2d_to_3d(p1, z=0, zdir="x")
+
+        ax.set_xlim(0, x)
+        ax.set_ylim(0, y)
+        ax.set_zlim(0, z)
+
+        plt.show()
+	
